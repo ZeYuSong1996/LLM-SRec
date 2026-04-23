542cf8d7dbad0bcb4203acc3125084915d35c4c3
szy <songzeyu@kuaishou.com>
2026-04-23 19:52:16 +0800

    mod params

--- SeqRec/sasrec/run_Industrial_and_Scientific.sh
+++ SeqRec/sasrec/run_Industrial_and_Scientific.sh
@@ -4,4 +4,4 @@ python main.py \
 --hf_local_dir /mmu_vcg2_wjc_ssd/songzeyu/Amazon-Reviews-2023 \
 --data_dir /mmu_vcg2_wjc_ssd/songzeyu/Amazon-Reviews-2023-trainval \
 --test_dir /mmu_vcg2_wjc_ssd/songzeyu/Amazon-Reviews-2023-test \
---dropout_rate 0.5
+--dropout_rate 0.1
