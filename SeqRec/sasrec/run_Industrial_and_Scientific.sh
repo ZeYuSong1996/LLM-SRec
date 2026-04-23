9c75481d234762744e53f754b651f2d085260b3d
szy <songzeyu@kuaishou.com>
2026-04-23 16:31:55 +0800

    add shell

---
+++ SeqRec/sasrec/run_Industrial_and_Scientific.sh
@@ -0,0 +1,6 @@
+python main.py \
+--device 1 \
+--dataset Industrial_and_Scientific \
+--hf_local_dir /mmu_vcg2_wjc_ssd/songzeyu/Amazon-Reviews-2023 \
+--data_dir /mmu_vcg2_wjc_ssd/songzeyu/Amazon-Reviews-2023-trainval \
+--test_dir /mmu_vcg2_wjc_ssd/songzeyu/Amazon-Reviews-2023-test
