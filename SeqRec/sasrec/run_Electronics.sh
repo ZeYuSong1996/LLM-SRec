d318f1f22074cb997d515faf90439c2997346501
szy <songzeyu@kuaishou.com>
2026-04-23 22:06:30 +0800

    add train

--- SeqRec/sasrec/run_Electronics.sh
+++ SeqRec/sasrec/run_Electronics.sh
@@ -1,8 +1,7 @@
 python main.py \
---device 1 \
---dataset Industrial_and_Scientific \
+--device 2 \
+--dataset Electronics \
 --hf_local_dir /mmu_vcg2_wjc_ssd/songzeyu/Amazon-Reviews-2023 \
 --data_dir /mmu_vcg2_wjc_ssd/songzeyu/Amazon-Reviews-2023-trainval \
 --test_dir /mmu_vcg2_wjc_ssd/songzeyu/Amazon-Reviews-2023-test \
---dropout_rate 0.1
-
+--dropout_rate 0.5
